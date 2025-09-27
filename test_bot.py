0ba92c0d6f7a328d09b62c2111ca7b6d27ccf473
andreimromanov <andrei.m.romanov@gmail.com>
2025-09-27 11:08:54 +0000

    test: add new unit test

--- test_bot.py
+++ test_bot.py
@@ -3,3 +3,8 @@ def test_reply_returns_string():
     from bot import reply
     assert isinstance(reply(), str)
 # added 2025-09-27T09:00:31.255403
+
+def test_reply_returns_string():
+    from bot import reply
+    assert isinstance(reply(), str)
+# added 2025-09-27T11:08:54.089743
