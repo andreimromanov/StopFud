1ecbe943c561a886ff09800f79c3af0870c63469
andreimromanov <andrei.m.romanov@gmail.com>
2025-09-30 11:01:46 +0000

    fix: improve bot reply logic

--- bot.py
+++ bot.py
@@ -15,3 +15,5 @@ if __name__ == "__main__":
 responses.append("research > rumors.")  # added 2025-09-27T19:10:01.735817
 
 responses.append("Stay strong, facts matters.")  # added 2025-09-29T21:13:24.212579
+
+responses.append("Stay strong, code matters.")  # added 2025-09-30T11:01:46.664285
